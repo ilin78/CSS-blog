b1d5a166c1495a3855fb8e949cc4a20272dba98f
dedmosay <ilinoa90@gmail.com>
2020-08-04 01:09:55 +0300

    Flex

---
+++ 1.path/16.FLEXBOX/flex-app/src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import './App.scss'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <div class="wraper">
+          <div class="block">
+            <div class="block__row">
+              <div class="block__column">
+                <div class="block__item">1</div>
+              </div>
+              <div class="block__column">
+                <div class="block__item">2</div>
+              </div>
+              <div class="block__column">
+                <div class="block__item">3</div>
+              </div>
+            </div>
+            <ul class="block__list">
+              <li>
+                <div class="block__info block__info_y">FLEX-контейнер</div>
+              </li>
+              <li>
+                <div class="block__info block__info_p">FLEX-элемент</div>
+              </li>
+              <li>
+                <div class="block__info block__info_g">Содержимое FLEX-элемента (контент)</div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
