042596e2b4127257bb8b094d79bcc26c9589c841
ilinoa <ilinoa90@gmail.com>
2020-08-17 13:22:36 +0300

    flex mobile

--- 1.path/17.Mobile Style/mobile-app/src/App.js
+++ 1.path/17.Mobile Style/mobile-app/src/App.js
@@ -8,22 +8,40 @@ var Blog = require("./img/blog.jpg");
 export default class App extends Component {
   render() {
     return (
-      <div class="wraper">
-        <div class="container">
-          <div class="header_row">
-            <a href="#" class="header__logo"></a>
-            <div class="header__menu menu">
-              <span></span>
+      <div className="wraper">
+        <div className="content">
+          <div className="container">
+          <div className="header__row">
+            <a href="#" className="header__logo"></a>
+            <div className="header__menu menu">
+              <span>Start</span>
             </div>
-            <nav class="menu__body">
-              <ul class="menu__list">
-                <li><a href="#" class="menu__link"></a></li>
-                <li><a href="#" class="menu__link"></a></li>
-                <li><a href="#" class="menu__link"></a></li>
-                <li><a href="#" class="menu__link"></a></li>
-                <li><a href="#" class="menu__link"></a></li>
+            <nav className="menu__body">
+              <ul className="menu__list">
+                <li><a href="#" className="menu__link">TEXT 1</a></li>
+                <li><a href="#" className="menu__link">TEXT 2</a></li>
+                <li><a href="#" className="menu__link">TEXT 3</a></li>
+                <li><a href="#" className="menu__link">TEXT 4</a></li>
+                <li><a href="#" className="menu__link">TEXT 5</a></li>
               </ul>
             </nav>
+           </div>
+          </div>
+          <div className="block">
+            <div className="container">
+              <div className="block__row">
+                <div className="block__title title"> Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  <div className="block__item">
+                    <div className="block__text">
+                      Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                      {/* Eos nesciunt dolores possimus voluptatum repellendus quidem */}
+                      {/* sint accusamus magnam iste quia neque, assumenda, obcaecati */}
+                      {/* adipisci expedita nihil fuga velit quae est. */}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
