b93edfbd044dd18e0aede3e4c349500c512242e4
ilinoa <ilinoa90@gmail.com>
2020-08-18 17:26:27 +0300

    add width min flex

--- 1.path/17.Mobile Style/README.md
+++ 1.path/17.Mobile Style/README.md
@@ -4,21 +4,26 @@
 #### Фиксированная верстка (не адаптивная).
 HTML 
 ```html
-<meta name="viewport" content="width=1170"/>
+<meta name="viewport" content="width=1170"/> 
 ```
 CSS
-
+задать ширину и выставить блок по центру
 ```css
 .container {
-  border: 2px solid red;
-  margin-bottom: 20px;
   width: 1170px;
-  
+  margin: 0px auto;
 } 
 ```
+Верстка изменяемая под ширину экрана.
+
+![](https://github.com/dedmosay/CSS-blog/blob/master/1.path/17.Mobile%20Style/image/min-width.jpg)
+
 #### Отзывчивая верстка (резиновая).
 ```css
-
+body {
+  min-width: 1170px;
+  margin: 0px auto;
+} 
 ```
 #### Адаптивная верстка (с брейкпоинтами)
 ```css
--- 1.path/17.Mobile Style/mobile-app/src/App.js
+++ 1.path/17.Mobile Style/mobile-app/src/App.js
@@ -11,52 +11,45 @@ export default class App extends Component {
       <div className="wraper">
         <div className="content">
           <div className="container">
-          <div className="header__row">
-            <a href="#" className="header__logo"></a>
-            <div className="header__menu menu">
-              <span>Start</span>
+            <div className="header__row">
+              <a href="#" className="header__logo"></a>
+              <div className="header__menu menu">
+                <span>Start</span>
+              </div>
+              <nav className="menu__body">
+                <ul className="menu__list">
+                  <li><a href="#" className="menu__link">TEXT 1</a></li>
+                  <li><a href="#" className="menu__link">TEXT 2</a></li>
+                  <li><a href="#" className="menu__link">TEXT 3</a></li>
+                  <li><a href="#" className="menu__link">TEXT 4</a></li>
+                  <li><a href="#" className="menu__link">TEXT 5</a></li>
+                </ul>
+              </nav>
             </div>
-            <nav className="menu__body">
-              <ul className="menu__list">
-                <li><a href="#" className="menu__link">TEXT 1</a></li>
-                <li><a href="#" className="menu__link">TEXT 2</a></li>
-                <li><a href="#" className="menu__link">TEXT 3</a></li>
-                <li><a href="#" className="menu__link">TEXT 4</a></li>
-                <li><a href="#" className="menu__link">TEXT 5</a></li>
-              </ul>
-            </nav>
-           </div>
           </div>
           <div className="block">
             <div className="container">
-              <div className="block__row">
-                <div className="block__title title"> Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  <div className="block__item">
-                    <div className="block__text">
-                      Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                      Eos nesciunt dolores possimus voluptatum repellendus quidem
-                      sint accusamus magnam iste quia neque, assumenda, obcaecati
-                      adipisci expedita nihil fuga velit quae est.
+              <div className="block__body">
+                <div className="block__row">
+                  <div className="block__column">
+                    <div className="block__item">
+                      <div className="block__title title">LoremLoremLoremLorem ipsum dolor sit amet consectetur, adipisicing</div>
+                      <div className="block__text">adipisci expedita nihil fuga velit quae est.</div>
+                      <div className="block__btn">Button</div>
                     </div>
                   </div>
-                </div>
-                <div className="block__title title"> Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  <div className="block__item">
-                    <div className="block__text">
-                      Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                      Eos nesciunt dolores possimus voluptatum repellendus quidem
-                      sint accusamus magnam iste quia neque, assumenda, obcaecati
-                      adipisci expedita nihil fuga velit quae est.
+                  <div className="block__column">
+                    <div className="block__item">
+                      <div className="block__title title"> Lorem ipsum dolor sit amet consectetur, adipisi</div>
+                      <div className="block__text"> sint accusamus magnam iste quia neque, assumenda,obcaecati adipisci expedita nihil fuga velit quae est. </div>
+                      <div className="block__btn">Button</div>
                     </div>
                   </div>
-                </div>
-                <div className="block__title title"> Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  <div className="block__item">
-                    <div className="block__text">
-                      Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                      Eos nesciunt dolores possimus voluptatum repellendus quidem
-                      sint accusamus magnam iste quia neque, assumenda, obcaecati
-                      adipisci expedita nihil fuga velit quae est.
+                  <div className="block__column">
+                    <div className="block__item">
+                      <div className="block__title title">lor sit amet consectetur, adipisicing</div>
+                      <div className="block__text"> Lorem ipsum dolor sit amet consectetur, adipisicingelit. Eos nesciunt dolores possimus voluptatum repellendus quidem sint accusamus magnam iste quia   velit quae est. </div>
+                      <div className="block__btn">Button</div>
                     </div>
                   </div>
                 </div>
