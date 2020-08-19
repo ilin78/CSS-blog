3cf17977224362d461cacb3c2fcb0afbf2e5f111
ilinoa <ilinoa90@gmail.com>
2020-08-19 17:44:06 +0300

    добавлена media верстка с брейконтами

--- 1.path/17.Mobile Style/README.md
+++ 1.path/17.Mobile Style/README.md
@@ -19,17 +19,55 @@ CSS
 ![](https://github.com/dedmosay/CSS-blog/blob/master/1.path/17.Mobile%20Style/image/min-width.jpg)
 
 #### Отзывчивая верстка (резиновая).
+Настраивается ```viewport```
+```html
+<meta name="viewport" content="width=device-width"/> 
+```
+
+ Указыается ширина в процентах, так как не зависимо от ширины экрана (от 4k до дисплея монитора 1080) страница должна отображаться корректно
 ```css
 body {
   min-width: 1170px;
   margin: 0px auto;
 } 
 ```
-#### Адаптивная верстка (с брейкпоинтами)
-```css
 
+[Responsinator](http://www.responsinator.com/) - сервис, который позволяет посмотреть, как будет выглядеть сайт на популярных устройствах.
+- iPhone eXpensive portrait · width: 375px
+- iPhone eXpensive landscape · width: 734px
+- Android (Pixel 2) portrait · width: 412px
+- Android (Pixel 2) landscape · width: 684px
+- iPhone 6-8 portrait · width: 375px
+- iPhone 6-8 landscape · width: 667px
+- iPhone 6-8 Plump portrait · width: 414px
+- iPhone 6-8 Plump landscape · width: 736px
+- iPad portrait · width: 768px
+- iPad landscape · width: 1024px
+
+![](https://github.com/dedmosay/CSS-blog/blob/master/1.path/17.Mobile%20Style/image/media.jpg)
+
+#### Адаптивная верстка (с брейкпоинтами)
+
+```css
+@media (max-width: 1170px){
+  .container{
+    max-width: 970px;
+  }
+}
+@media (max-width: 992px){
+  .container{
+    max-width: 750px;
+  }
+}
+@media (max-width: 767px){
+  .container{
+    max-width: none;
+  }
+}
 ```
+
 #### Отзывчиво-адаптивная верстка (резиновая с брейкпоинтами)
+
 ```css
 
 ```
--- 1.path/17.Mobile Style/mobile-app/src/App.js
+++ 1.path/17.Mobile Style/mobile-app/src/App.js
@@ -8,21 +8,18 @@ var Blog = require("./img/blog.jpg");
 export default class App extends Component {
   render() {
     return (
-      <div className="wraper">
-        <div className="content">
-          <div className="container">
+      <div className="wrapper">
+        <div className="content-full">
+          <div className="container ">
             <div className="header__row">
-              <a href="#" className="header__logo"></a>
-              <div className="header__menu menu">
-                <span>Start</span>
-              </div>
+              <div className="header__logo">Production</div>
+              <div className="header__menu menu"></div>
               <nav className="menu__body">
                 <ul className="menu__list">
-                  <li><a href="#" className="menu__link">TEXT 1</a></li>
-                  <li><a href="#" className="menu__link">TEXT 2</a></li>
-                  <li><a href="#" className="menu__link">TEXT 3</a></li>
-                  <li><a href="#" className="menu__link">TEXT 4</a></li>
-                  <li><a href="#" className="menu__link">TEXT 5</a></li>
+                  <li><a href="#" className="menu__link">Turn on</a></li>
+                   <li><a href="#" className="menu__link">News</a></li>
+                  <li><a href="#" className="menu__link">Support</a></li>
+                  <li><a href="#" className="menu__link">About</a></li>
                 </ul>
               </nav>
             </div>
@@ -33,23 +30,23 @@ export default class App extends Component {
                 <div className="block__row">
                   <div className="block__column">
                     <div className="block__item">
-                      <div className="block__title title">LoremLoremLoremLorem ipsum dolor sit amet consectetur, adipisicing</div>
-                      <div className="block__text">adipisci expedita nihil fuga velit quae est.</div>
-                      <div className="block__btn">Button</div>
+                      <div className="block__title title">Our production</div>
+                      <div className="block__text">In a basis of manufacture of our production - the best engineering decisions and the high technologies which allow to achieve the perfect result.</div>
+                      <div className="block__btn">See more</div>
                     </div>
                   </div>
                   <div className="block__column">
                     <div className="block__item">
-                      <div className="block__title title"> Lorem ipsum dolor sit amet consectetur, adipisi</div>
-                      <div className="block__text"> sint accusamus magnam iste quia neque, assumenda,obcaecati adipisci expedita nihil fuga velit quae est. </div>
-                      <div className="block__btn">Button</div>
+                      <div className="block__title title"> Technology</div>
+                      <div className="block__text">Technology has evolved and shaped our workplaces in many ways, through the adoption of tools like the internet and email for communications, word processing, spreadsheets and presentations for office productivity, electronic databases for record keeping, and robots and artificial intelligence for automation</div>
+                      <div className="block__btn">Start now</div>
                     </div>
                   </div>
                   <div className="block__column">
                     <div className="block__item">
-                      <div className="block__title title">lor sit amet consectetur, adipisicing</div>
-                      <div className="block__text"> Lorem ipsum dolor sit amet consectetur, adipisicingelit. Eos nesciunt dolores possimus voluptatum repellendus quidem sint accusamus magnam iste quia   velit quae est. </div>
-                      <div className="block__btn">Button</div>
+                      <div className="block__title title">Support</div>
+                      <div className="block__text">If you still have questions call us or fill in a short questionnaire on website development.</div>
+                      <div className="block__btn">Send</div>
                     </div>
                   </div>
                 </div>
