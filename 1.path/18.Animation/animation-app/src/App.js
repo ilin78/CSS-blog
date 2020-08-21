4e9181a46668b8f050fb6fce935aba2aadf48e33
ilinoa <ilinoa90@gmail.com>
2020-08-21 12:30:18 +0300

    anime

---
+++ 1.path/18.Animation/animation-app/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./App.scss";
+
+var myBlog = require("./img/myBlog.jpg");
+var What = require("./img/what.jpg");
+var Blog = require("./img/blog.jpg");
+
+export default class App extends Component {
+  render() {
+    return (
+      
+    );
+  }
+}
