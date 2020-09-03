bd66fde845bd5cfb913dd66e2b1afca4fc3f001c
ilinoa <ilinoa90@gmail.com>
2020-09-03 18:08:44 +0300

    3.practics

--- 1.path/19.Grid/3.Practice/grid-app/src/App.js
+++ 1.path/19.Grid/3.Practice/grid-app/src/App.js
@@ -1,41 +1,73 @@
 import React, { Component } from "react";
 import "./App.scss";
+var Logo = require("./img/logo.jpg");
 
 export default class App extends Component {
   render() {
     return (
-      <div className="wrapper">
-        <div className="content">
-        <h1>Grid-Layout - Определение сетки</h1>
-        <div className="grid">
-          <div className="grid__body">
-            <div className="grid__item">1</div>
-            <div className="grid__item">2</div>
-            <div className="grid__item">3</div>
-            <div className="grid__item">4</div>
-            <div className="grid__item">5</div>
-            <div className="grid__item">6</div>
+      <div class="wrapper">
+        {/* HEAD */}
+        <header class="header">
+          <div  class="header__logo" >
+            <a href="#"><img  src={Logo} alt="" ></img></a>
           </div>
-        </div>
-        <h2>Имена областей</h2>
-        <div className="grid-wrapper">
-          <header className="grid-header">Шапка</header>
-          <aside className="grid-side">Сайдбар</aside>
-          <main className="grid-content">Контент</main>
-        </div>
-        <h2>Неявная сетка</h2>
-        <div className="grid-auto">
-            <div className="grid-auto__item">1</div>
-            <div className="grid-auto__item">2</div>
-            <div className="grid-auto__item">3</div>
-            <div className="grid-auto__item">4</div>
-            <div className="grid-auto__item">5</div>
-            <div className="grid-auto__item">6</div>
-            <div className="grid-auto__item">7</div>
-            
-        </div>
-          <h6> 7 - неявно заданный элемент</h6>  
-        </div>
+          <nav class="header__menu">
+              <ul class="header__list">
+                <li>
+                  <a href="" class="header__link">Пункт меню</a>
+                </li>
+                <li>
+                  <a href="" class="header__link">Пункт меню</a>
+                </li>
+                <li>
+                  <a href="" class="header__link">Пункт меню</a>
+                </li>
+                <li>
+                  <a href="" class="header__link">Пункт меню</a>
+                </li>
+              </ul>
+              <div class="header__burger">
+                <span> </span>
+              </div>
+          </nav>
+        </header>
+        {/* CONTENT */}
+        <main class="main">
+          <aside class="sidebar">
+            <nav class="sidebar__menu">
+              <ul class="sidebar__list">
+                <li>
+                  <a href="" class="sidebar__link">Пункт меню</a>
+                </li>
+                <li>
+                  <a href="" class="sidebar__link">Пункт меню</a>
+                </li>
+                <li>
+                  <a href="" class="sidebar__link">Пункт меню</a>
+                </li>
+                <li>
+                  <a href="" class="sidebar__link">Пункт меню</a>
+                </li>
+              </ul>
+            </nav>
+          </aside>
+
+          <section class="content"> 
+            <h1 class="content__title">Grid-Layout - Практика</h1>
+            <section class="content__gallary gallary">
+              <h2 class="gallary__title">Галерея</h2>
+              <div class="gallary__items">
+                <div class="gallary__item">
+
+                </div>
+              </div>
+            </section>
+          </section>
+        </main>
+        {/* FOOTER */}
+        <footer class="footer">
+
+        </footer>
       </div>
     );
   }
