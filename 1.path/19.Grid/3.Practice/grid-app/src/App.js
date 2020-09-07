c09e1e99d447f1d43fab65063a2b645177d3f826
ilinoa <ilinoa90@gmail.com>
2020-09-07 18:13:16 +0300

    grid practics

--- 1.path/19.Grid/3.Practice/grid-app/src/App.js
+++ 1.path/19.Grid/3.Practice/grid-app/src/App.js
@@ -1,6 +1,17 @@
 import React, { Component } from "react";
 import "./App.scss";
 var Logo = require("./img/logo.jpg");
+var img_01 = require("./img/1.jpg");
+var img_02 = require("./img/2.jpg");
+var img_03 = require("./img/3.jpg");
+var img_04 = require("./img/4.jpg");
+var img_05 = require("./img/5.jpg");
+var img_06 = require("./img/6.jpg");
+var img_07 = require("./img/7.jpg");
+var img_08 = require("./img/8.jpg");
+var img_09 = require("./img/9.jpg");
+var img_10 = require("./img/10.jpg");
+var img_fls = require("./img/fls.jpg");
 
 export default class App extends Component {
   render() {
@@ -9,7 +20,9 @@ export default class App extends Component {
         {/* HEAD */}
         <header class="header">
           <div  class="header__logo" >
-            <a href="#"><img  src={Logo} alt="" ></img></a>
+            <a href="#">
+              {/* <img  src={Logo} alt="" ></img> */}
+              </a>
           </div>
           <nav class="header__menu">
               <ul class="header__list">
@@ -57,16 +70,78 @@ export default class App extends Component {
             <section class="content__gallary gallary">
               <h2 class="gallary__title">Галерея</h2>
               <div class="gallary__items">
-                <div class="gallary__item">
-
+                <div class="gallary__item gallary_item_big">
+                  <img class="width" src={img_01} alt="" ></img>
+                </div>
+                <div class="gallary__item gallary_item_big">
+                  <img class="width" src={img_02} alt="" ></img>
+                </div>
+                <div class="gallary__item gallary_item_big">
+                  <img class="width" src={img_03} alt="" ></img>
+                </div>
+                <div class="gallary__item gallary_item_big">
+                  <img class="width" src={img_04} alt="" ></img>
+                </div>
+                <div class="gallary__item gallary_item_big">
+                  <img class="width" src={img_05} alt="" ></img>
+                </div>
+                <div class="gallary__item gallary_item_big">
+                  <img class="width" src={img_06} alt="" ></img>
+                </div>
+                <div class="gallary__item gallary_item_big">
+                  <img class="width" src={img_07} alt="" ></img>
+                </div>
+                <div class="gallary__item gallary_item_big">
+                  <img class="width" src={img_08} alt="" ></img>
+                </div>
+                <div class="gallary__item gallary_item_big">
+                  <img class="width" src={img_09} alt="" ></img>
+                </div>
+                <div class="gallary__item gallary_item_big">
+                  <img class="width" src={img_10} alt="" ></img>
+                </div>
+              </div>
+              </section>
+              <div class="content__products products">
+                <h2 class="products__title">Товары</h2>
+                <div class="products__items">
+                  <div class="products__item">
+                    <a class="products__image">
+                      <img  class="width" src={img_fls} alt="" ></img>
+                    </a>
+                    <a href="" class="products__name">Мы нацелены попасть в центр!</a>
+                    <a href="" class="products__button">Присоединиться</a>
+                  </div>
+                </div>
+                <div class="products__items">
+                  <div class="products__item">
+                    <a class="products__image">
+                      <img  class="width" src={img_fls} alt="" ></img>
+                    </a>
+                    <a href="" class="products__name">Мы нацелены попасть в центр!</a>
+                    <a href="" class="products__button">Присоединиться</a>
+                  </div>
+                </div>
+                <div class="products__items">
+                  <div class="products__item">
+                    <a class="products__image">
+                      <img  class="width" src={img_fls} alt="" ></img>
+                    </a>
+                    <a href="" class="products__name">Мы нацелены попасть в центр!</a>
+                    <a href="" class="products__button">Присоединиться</a>
+                  </div>
                 </div>
               </div>
             </section>
-          </section>
         </main>
         {/* FOOTER */}
         <footer class="footer">
-
+          <div class="footer__copy">
+            Copy, 2020 
+          </div>
+          <div class="footer__text">
+            Чтобы продолжить нажмите "Присоединиться"
+          </div>
         </footer>
       </div>
     );
